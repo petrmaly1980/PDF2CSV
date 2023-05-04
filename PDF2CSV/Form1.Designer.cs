a22e6a57c2c6a3a8f1a2f93595ad5fa09c1a6035
Petr Maly <petr.maly1980@gmail.com>
2023-05-04 12:32:11 +0200

    Add project files.

---
+++ PDF2CSV/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace PDF2CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInputFolder = new System.Windows.Forms.TextBox();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnSelectInputFolder = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSelectOutputFolder = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input PDF Folder:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output Folder:";
+            // 
+            // txtInputFolder
+            // 
+            this.txtInputFolder.Location = new System.Drawing.Point(108, 32);
+            this.txtInputFolder.Name = "txtInputFolder";
+            this.txtInputFolder.Size = new System.Drawing.Size(284, 20);
+            this.txtInputFolder.TabIndex = 2;
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Location = new System.Drawing.Point(108, 68);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.Size = new System.Drawing.Size(284, 20);
+            this.txtOutputFolder.TabIndex = 3;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(108, 103);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(284, 23);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnSelectInputFolder
+            // 
+            this.btnSelectInputFolder.Location = new System.Drawing.Point(398, 30);
+            this.btnSelectInputFolder.Name = "btnSelectInputFolder";
+            this.btnSelectInputFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectInputFolder.TabIndex = 6;
+            this.btnSelectInputFolder.Text = "Select";
+            this.btnSelectInputFolder.UseVisualStyleBackColor = true;
+            this.btnSelectInputFolder.Click += new System.EventHandler(this.btnOpenInputFolder_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(105, 150);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status";
+            // 
+            // btnSelectOutputFolder
+            // 
+            this.btnSelectOutputFolder.Location = new System.Drawing.Point(398, 66);
+            this.btnSelectOutputFolder.Name = "btnSelectOutputFolder";
+            this.btnSelectOutputFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectOutputFolder.TabIndex = 7;
+            this.btnSelectOutputFolder.Text = "Select";
+            this.btnSelectOutputFolder.UseVisualStyleBackColor = true;
+            this.btnSelectOutputFolder.Click += new System.EventHandler(this.btnSelectOutputFolder_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(383, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Petr Malý ver. 1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 172);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSelectOutputFolder);
+            this.Controls.Add(this.btnSelectInputFolder);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtOutputFolder);
+            this.Controls.Add(this.txtInputFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PDF2CSV Convertor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInputFolder;
+        private System.Windows.Forms.TextBox txtOutputFolder;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnSelectInputFolder;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSelectOutputFolder;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PDF2CSV/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Spire.Pdf;
+using System.IO;
+using System.Windows.Forms;
+using Spire.Pdf.Texts;
+using Spire.Pdf.Grid;
+
+namespace PDF2CSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string inputFolder = txtInputFolder.Text;
+            string outputFolder = txtOutputFolder.Text;
+            
+            if (string.IsNullOrEmpty(inputFolder) || string.IsNullOrEmpty(outputFolder))
+            {
+                lblStatus.Text = "Please provide input and output folders.";
+                return;
+            }
+
+            if (!Directory.Exists(inputFolder))
+            {
+                lblStatus.Text = "Input folder does not exist.";
+                return;
+            }
+
+            if (!Directory.Exists(outputFolder))
+            {
+                lblStatus.Text = "Output folder does not exist.";
+                return;
+            }
+
+            string[] pdfFiles = Directory.GetFiles(inputFolder, "*.pdf");
+            foreach (string pdfFile in pdfFiles)
+            {
+                try
+                {
+                    string csvFile = Path.Combine(outputFolder, Path.GetFileNameWithoutExtension(pdfFile) + ".xlsx");
+                    //Create a PdfDocument instance
+                    PdfDocument document = new PdfDocument();
+                    //Load a PDF file
+                    document.LoadFromFile(pdfFile);
+                    //Save to Excel
+                    document.SaveToFile(csvFile, FileFormat.XLSX);
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text = "Error converting " + pdfFile + ": " + ex.Message;
+                }
+            }
+
+            // Show a message box to indicate that the conversion is complete
+            MessageBox.Show("Conversion complete.", "PDF to CSV", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public FolderBrowserDialog fdialog = new FolderBrowserDialog();
+        //Default Open Folder
+        string myFolder = @"C:\PDF";
+        private void btnOpenInputFolder_Click(object sender, EventArgs e)
+        {
+            fdialog.SelectedPath = myFolder;
+            if (fdialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                
+                txtInputFolder.Text = fdialog.SelectedPath;
+                txtOutputFolder.Text = fdialog.SelectedPath;
+            }
+            else
+            {
+                txtInputFolder.Text = "";
+            }
+        }
+        private void btnSelectOutputFolder_Click(object sender, EventArgs e)
+        {
+            
+            FolderBrowserDialog fdialog = new FolderBrowserDialog();
+            if (fdialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtInputFolder.Text = fdialog.SelectedPath;
+            }
+            else
+            {
+                //If not selected default folder is Input Folder
+                txtInputFolder.Text = txtInputFolder.Text;
+            }
+        }
+    }
+}
